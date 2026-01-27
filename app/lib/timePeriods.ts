f24076e4dda413011656a14b92e5169592e60a5b
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2026-01-27 15:10:22 -0500

    changed sign in logic to trigger every 3 days now

--- app/lib/timePeriods.ts
+++ app/lib/timePeriods.ts
@@ -5,10 +5,11 @@ export const TIME_PERIODS = {
   MINUTE: 60 * 1000,        // 1 minute
   HOUR: 60 * 60 * 1000,     // 1 hour
   DAY: 24 * 60 * 60 * 1000, // 1 day
+  THREE_DAYS: 3 * 24 * 60 * 60 * 1000, // 3 days
   WEEK: 7 * 24 * 60 * 60 * 1000, // 1 week
 } as const
 
 // Default time period to check for redirecting to pricing page
 // Change this to TIME_PERIODS.MINUTE, HOUR, DAY, or WEEK
-export const PRICING_REDIRECT_THRESHOLD = TIME_PERIODS.MINUTE
+export const PRICING_REDIRECT_THRESHOLD = TIME_PERIODS.THREE_DAYS
 
